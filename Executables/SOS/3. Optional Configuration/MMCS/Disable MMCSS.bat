2f7861eda4798da29580e26e1f64594a8a7a9048
Chr <98983742+u-chr@users.noreply.github.com>
2026-05-04 20:08:45 +0300

    Delete Executables/SOS/3. Optional Configuration/MMCS directory

--- Executables/SOS/3. Optional Configuration/MMCS/Disable MMCSS.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-Reg add "HKLM\SYSTEM\CurrentControlSet\Services\MMCSS" /v "Start" /t REG_DWORD /d "4" /f >nul
-pause
--- Executables/SOS/3. Optional Configuration/MMCS/Enable MMCSS (Default).bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-Reg add "HKLM\SYSTEM\CurrentControlSet\Services\MMCSS" /v "Start" /t REG_DWORD /d "2" /f >nul
-pause 
