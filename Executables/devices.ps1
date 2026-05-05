f01c4bcb0b99ba8670c56e54c1e38ef7e15ecb6c
Chr <98983742+u-chr@users.noreply.github.com>
2026-05-05 18:17:24 +0300

    disabled the wrong device

--- Executables/devices.ps1
+++ Executables/devices.ps1
@@ -1,6 +1,6 @@
 $devices = @(
     "Composite Bus Enumerator",
-    "NDIS Virtual Network Adapter Enumerator",
+    "Microsoft RRAS Root Enumerator",
     "Microsoft GS Wavetable Synth"
 )
 Get-PnpDevice -FriendlyName $devices -ErrorAction Ignore | Disable-PnpDevice -Confirm:$false -ErrorAction Ignore
