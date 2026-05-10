e24fcf7d7efdfcba77deeb2588568e8dd83a3a80
Chr <98983742+u-chr@users.noreply.github.com>
2026-05-10 19:10:24 +0300

    Update xbox.bat

--- Executables/xbox.bat
+++ Executables/xbox.bat
@@ -1,32 +1,9 @@
 @echo off
-setLOCAL ENABLEDELAYEDEXPANSION
-
-if "%1"=="disable" set state=disable
-if "%1"=="enable" set state=enable
-
-
-if /i "%state%"=="disable" (
-    :: disable xbox services
-    for %%z in (
-    XblAuthManager
-    XblGameSave 
-    XboxGipSvc
-    XboxNetApiSvc
-    DoSvc
+for %%z in (
+  XblAuthManager
+  XblGameSave
+  XboxGipSvc
+  XboxNetApiSvc
 ) do (
-    Reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\%%z" /v "Start" /t REG_DWORD /d "4" /f 
-)
-)
-
-if /i "%state%"=="enable" (
-    :: enable xbox services
-    for %%z in (
-    XblAuthManager
-    XblGameSave 
-    XboxGipSvc
-    XboxNetApiSvc
-    DoSvc
-) do (
-    Reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\%%z" /v "Start" /t REG_DWORD /d "2" /f 
-)
+  Reg add "HKLM\SYSTEM\CurrentControlSet\Services\%%z" /v "Start" /t REG_DWORD /d 4 /f
 )
